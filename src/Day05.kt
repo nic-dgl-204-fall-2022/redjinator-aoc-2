41b4f5448e4e939a347676820c3ec4854740f0e4
Redjinator <rmcpherson@northislandcollege.ca>
2022-11-11 16:28:10 -0800

    Complete: aoc 2015 day 5 part 2

--- src/Day05.kt
+++ src/Day05.kt
@@ -10,25 +10,50 @@ fun main() {
         fun String.hasVowels()      = this.count { it in "aeiou" } >= 3
         fun String.hasLetterTwice() = this.zipWithNext().any { it.first == it.second }
 
+
         // Checks string eligibility for nice list
-        fun isNice(str: String): Boolean {
-            return !str.hasUnwantedStrings() && str.hasVowels() && str.hasLetterTwice()
-        }
+        fun isNice(str: String) = !str.hasUnwantedStrings() && str.hasVowels() && str.hasLetterTwice()
 
         var totalNiceList = 0
 
         for(string in input) {
             when(isNice(string)) {
-                true -> totalNiceList++
-                false -> continue
+                true        -> totalNiceList++
+                false       -> continue
             }
         }
 
         return totalNiceList
     }
 
-    // Part 1
-    val input = readInput("input")
-    println(part1(input))
 
+
+
+
+    fun part2(input: List<String>): Int {
+
+        // Criteria for nice list
+        val pattern = Regex("(..).*\\1")
+        fun String.equalEnds() = this.windowed(3).any { it.first() == it.last() }
+        fun String.checkForPairs() = this.windowed(2).count() > this.windowed(2).distinct().count()
+
+        // Nice list check
+        fun isNice(str: String) = pattern.containsMatchIn(str) && str.checkForPairs() && str.equalEnds()
+
+        var totalNiceList   = 0
+
+        for(string in input) {
+            when(isNice(string)) {
+                true        -> totalNiceList++
+                false       -> continue
+            }
+        }
+
+        return totalNiceList
+    }
+
+    val input = readInput("input")
+
+    println(part1(input))
+    println(part2(input))
 }
